0884550cce9df40624cb46940f31dec092006823
Lorence <hernandez.lorence@yahoo.com>
2017-02-12 04:30:37 -0800

    Update ingame.cpp

--- ingame.cpp
+++ ingame.cpp
@@ -28,62 +28,70 @@ void InGame::draw()
 
 void InGame::update(double delta)
 {
+    std::cout << enemies.size() << std::endl;
     //mouseHolding->print();
-    static Wave::waveInfo currWave = wave.getNextWave();
-    if ( !currWave.hasFinishedSpawning() )
-    {
-        if ( timerForSpawn.getElapsedTime().asSeconds() >= currWave.timePerSpawn )
+        static Wave::waveInfo currWave = wave.getNextWave();
+        if ( !currWave.hasFinishedSpawning() )
         {
-            enemies.emplace_back(std::make_shared<Enemy>(currWave.enemy+".png",startingPos,currMap[0].position,boundaries[INFORMATION]));
-            currWave.spawn();
-            timerForSpawn.restart();
-            timerForWave.restart();
-        }
-    }
-    else
-    {
-        if ( timerForWave.getElapsedTime().asSeconds() >= wave.timePerWave )
-        {
-            currWave = wave.getNextWave();
-        }
-    }
-    for ( int a = 0; a < enemies.size(); a++ )
-    {
-        if ( !enemies[a]->isAlive() )
-        {
-            playerGold += enemies[a]->bounty;
-            score++;
-            infoScore.setText("Score: " + dtostr(score));
-            infoPlayerGold.setText("Gold: " + dtostr(playerGold));
-            enemies.erase(enemies.begin()+a);
-
-        }
-    }
-    for ( std::size_t a = 0; a < enemies.size(); a++ )
-    {
-        if ( enemies[a]->isAlive() )
-        {
-            if ( enemies[a]->hasReachedGoal() )
+            if ( timerForSpawn.getElapsedTime().asSeconds() >= currWave.timePerSpawn )
             {
-                enemies[a]->incrementGoalIndex();
-                if ( enemies[a]->getGoalIndexQuad() == leakPoint+4 )
-                {
-                    enemies.erase(enemies.begin()+a);
-                }
-                enemies[a]->setGoalPoint(currMap[enemies[a]->getGoalIndexQuad()].position);
+                enemies.emplace_back(std::make_shared<Enemy>(currWave.enemy+".png",startingPos,currMap[0].position,boundaries[INFORMATION]));
+                currWave.spawn();
+                timerForSpawn.restart();
+                timerForWave.restart();
             }
-            enemies[a]->run(delta);
         }
-    }
-    for ( std::size_t a = 0; a < towers.size(); a++ )
-    {
-        towers[a].updateBullet(delta);
-        if ( !enemies.empty() )
+        else
         {
-            towers[a].findTarget(enemies);
-            towers[a].changeRadiusColor(enemies);
+            if ( timerForWave.getElapsedTime().asSeconds() >= wave.timePerWave )
+            {
+                if ( wave.isAllWaveFinished() )
+                {
+                    currentInformation.setText("all wave is finished!");
+                    isGameOver = true;
+                    return;
+                }
+                currWave = wave.getNextWave();
+            }
         }
-    }
+        for ( int a = 0; a < enemies.size(); a++ )
+        {
+            if ( !enemies[a]->isAlive() )
+            {
+                playerGold += enemies[a]->bounty;
+                score++;
+                infoScore.setText("Score: " + dtostr(score));
+                infoPlayerGold.setText("Gold: " + dtostr(playerGold));
+                enemies.erase(enemies.begin()+a);
+
+            }
+        }
+        for ( std::size_t a = 0; a < enemies.size(); a++ )
+        {
+            if ( enemies[a]->isAlive() )
+            {
+                if ( enemies[a]->hasReachedGoal() )
+                {
+                    enemies[a]->incrementGoalIndex();
+                    if ( enemies[a]->getGoalIndexQuad() == leakPoint+4 )
+                    {
+                        enemies.erase(enemies.begin()+a);
+                    }
+                    enemies[a]->setGoalPoint(currMap[enemies[a]->getGoalIndexQuad()].position);
+                }
+                enemies[a]->run(delta);
+            }
+        }
+        for ( std::size_t a = 0; a < towers.size(); a++ )
+        {
+            towers[a].updateBullet(delta);
+            if ( !enemies.empty() )
+            {
+                towers[a].findTarget(enemies);
+                towers[a].changeRadiusColor(enemies);
+            }
+        }
+
 }
 
 Location InGame::getMouseLocation()
@@ -109,80 +117,80 @@ void InGame::syncTileToMouse(Entity& ent)
 
 void InGame::event_handler(sf::Event e)
 {
-    if ( e.type == sf::Event::MouseMoved )
-    {
-        if ( mouseHolding )
+        if ( e.type == sf::Event::MouseMoved )
         {
-            syncTileToMouse(*mouseHolding);
-            syncTileToMouse(emptyTile);
-        }
-    }
-    else if ( e.type == sf::Event::KeyPressed )
-    {
-        if ( e.key.code == sf::Keyboard::Space )
-        {
-            enemies.emplace_back(std::make_shared<Enemy>("bandit.png",startingPos,currMap[0].position,boundaries[INFORMATION]));
-        }
-    }
-    else if ( e.type == sf::Event::MouseButtonPressed )
-    {
-        if ( e.mouseButton.button == sf::Mouse::Left )
-        {
-            Location loc = getMouseLocation();
-
-            if ( loc == Location::SHOP )
+            if ( mouseHolding )
             {
-                shop.event_handler(e, sf::Mouse::getPosition(gameWindow));
-                *mouseHolding = shop.getSelectedTower();
+                syncTileToMouse(*mouseHolding);
+                syncTileToMouse(emptyTile);
             }
-            else if ( loc == Location::MAP )
+        }
+        else if ( e.type == sf::Event::KeyPressed )
+        {
+            if ( e.key.code == sf::Keyboard::Space )
             {
-                if ( shop.hasSelectedProduct() )
+                enemies.emplace_back(std::make_shared<Enemy>("bandit.png",startingPos,currMap[0].position,boundaries[INFORMATION]));
+            }
+        }
+        else if ( e.type == sf::Event::MouseButtonPressed )
+        {
+            if ( e.mouseButton.button == sf::Mouse::Left )
+            {
+                Location loc = getMouseLocation();
+
+                if ( loc == Location::SHOP )
                 {
-                    if ( shop.buySelectedTower(playerGold) )
+                    shop.event_handler(e, sf::Mouse::getPosition(gameWindow));
+                    *mouseHolding = shop.getSelectedTower();
+                }
+                else if ( loc == Location::MAP )
+                {
+                    if ( shop.hasSelectedProduct() )
                     {
-                        towers.emplace_back(shop.getSelectedTower(),false);
-                        syncTileToMouse(towers.back());
-                        infoPlayerGold.setText(dtostr(playerGold));
+                        if ( shop.buySelectedTower(playerGold) )
+                        {
+                            towers.emplace_back(shop.getSelectedTower(),false);
+                            syncTileToMouse(towers.back());
+                            infoPlayerGold.setText(dtostr(playerGold));
+                        }
+                        else
+                        {
+                            alert.setText("NOT ENOUGH GOLD!");
+                            currentInformation = alert;
+                        }
+                        *mouseHolding = emptyTile;
+                        shop.returnProduct();
                     }
                     else
                     {
-                        alert.setText("NOT ENOUGH GOLD!");
-                        currentInformation = alert;
+                        for ( int a = 0; a < towers.size(); a++ )
+                        {
+                            if ( towers[a].isMouseInside(sf::Mouse::getPosition(gameWindow)))
+                            {
+                                currentInformation = towers[a].getInformation();
+                                towers[a].flipVisibility();
+                                break;
+                            }
+                        }
+                        for ( int a  = 0; a < enemies.size(); a++ )
+                        {
+                            if ( enemies[a]->isMouseInside(sf::Mouse::getPosition(gameWindow)) )
+                            {
+                                currentInformation = enemies[a]->getInformation();
+                                break;
+                            }
+                        }
                     }
+                }
+            }
+            else if ( e.mouseButton.button == sf::Mouse::Right )
+            {
+                if ( shop.hasSelectedProduct() )
+                {
                     *mouseHolding = emptyTile;
                     shop.returnProduct();
                 }
-                else
-                {
-                    for ( int a = 0; a < towers.size(); a++ )
-                    {
-                        if ( towers[a].isMouseInside(sf::Mouse::getPosition(gameWindow)))
-                        {
-                            currentInformation = towers[a].getInformation();
-                            towers[a].flipVisibility();
-                            break;
-                        }
-                    }
-                    for ( int a  = 0; a < enemies.size(); a++ )
-                    {
-                        if ( enemies[a]->isMouseInside(sf::Mouse::getPosition(gameWindow)) )
-                        {
-                            currentInformation = enemies[a]->getInformation();
-                            break;
-                        }
-                    }
-                }
             }
         }
-        else if ( e.mouseButton.button == sf::Mouse::Right )
-        {
-            if ( shop.hasSelectedProduct() )
-            {
-                *mouseHolding = emptyTile;
-                shop.returnProduct();
-            }
-        }
-    }
 
 }
